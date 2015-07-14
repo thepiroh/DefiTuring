15d891b51f3c2898a85b5de54a53f75f6353f4bf
Antoine Benquet <antoinebenquet@icloud.com>
2015-07-14 18:10:15 +0200

    Create problem2.py

---
+++ 2/problem2.py
@@ -0,0 +1,22 @@
+def problem2():
+    
+    def F(n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return F(n-1)+F(n-2)
+    
+    
+    liste = []
+    i=0
+    while F(i) < 4000000:
+        if F(i)%2 != 0:
+            print(F(i))
+            liste.append(F(i))
+        i+=1
+    #print(liste)
+    print(sum(liste))
+
+problem2()
